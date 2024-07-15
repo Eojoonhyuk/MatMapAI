d8c7249102e539b0a44600d7a25ec284fefadde1
Eojoonhyuk <anftlcb@gmail.com>
2024-07-15 17:12:36 +0900

    prompt 초기 설정

--- app/services/openai.action.ts
+++ app/services/openai.action.ts
@@ -5,3 +5,5 @@ import OpenAI from "openai";
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY || "",
 });
+
+export const createAnswer = async (_: any, formData: FormData) => {};
