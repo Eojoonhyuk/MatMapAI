fbc2867ec376d60192f772a6c45d25b2d0389c3f
Eojoonhyuk <anftlcb@gmail.com>
2024-08-08 23:37:59 +0900

    style: flex 수정

--- app/components/Answer.tsx
+++ app/components/Answer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useFormStatus } from "react-dom";
 import { KakaoMap } from "./KakaoMap";
 
 interface AnswerProps {
@@ -14,34 +13,29 @@ interface RecommenedFoods {
 
 export const Answer = ({ answer }: AnswerProps) => {
   const [selectedFood, setSelectedFood] = useState("");
-  const { pending } = useFormStatus();
   const recommenedFoods: RecommenedFoods = JSON.parse(answer);
 
   return (
     <div className="flex flex-col gap-3">
-      {pending ? (
-        <span>답변 생성중</span>
-      ) : (
-        <div>
-          <ul className="flex gap-3 flex-wrap w-96">
-            {recommenedFoods.foods.map((food) => (
-              <li
-                key={food}
-                className="w-fit border rounded-lg bg-slate-100 text-sm"
+      <div>
+        <ul className="flex gap-3 flex-wrap w-96">
+          {recommenedFoods.foods.map((food) => (
+            <li
+              key={food}
+              className="w-fit border rounded-lg bg-slate-100 text-sm"
+            >
+              <button
+                className="p-2"
+                type="button"
+                onClick={() => setSelectedFood(food)}
               >
-                <button
-                  className="p-2"
-                  type="button"
-                  onClick={() => setSelectedFood(food)}
-                >
-                  {food}
-                </button>
-              </li>
-            ))}
-          </ul>
-          <KakaoMap keyword={selectedFood} />
-        </div>
-      )}
+                {food}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <KakaoMap keyword={selectedFood} />
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { QuestionForm } from "./components/QuestionForm";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex">
-      <div className="w-1/3 flex justify-center items-center flex-col gap-5">
+    <main className="w-full min-h-screen flex justify-center items-center">
+      <div className="w-full flex justify-center items-center flex-col gap-5">
         <Logo />
         <QuestionForm />
       </div>
