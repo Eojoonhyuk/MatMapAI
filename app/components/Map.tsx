4cc9d1a10c78256dfa7cbee1c81a647f39b4d154
Eojoonhyuk <anftlcb@gmail.com>
2024-08-23 16:43:41 +0900

    chore: useState center 삭제

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -13,7 +13,6 @@ import { Marker } from "./Marker";
 
 export const Map = ({ keyword }: { keyword: string }) => {
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const [center, setCenter] = useState<google.maps.LatLngLiteral | null>(null);
   const [places, setPlaces] = useState<google.maps.places.Place[] | null>(null);
 
   const onLoad = useCallback((map: google.maps.Map) => {
@@ -32,7 +31,7 @@ export const Map = ({ keyword }: { keyword: string }) => {
       textQuery: text,
       fields: ["displayName", "location"],
       locationBias: {
-        center: center || defaultMapCenter,
+        center: map?.getCenter() || defaultMapCenter,
         radius: 2000,
       },
       includedType: "restaurant",
@@ -46,6 +45,19 @@ export const Map = ({ keyword }: { keyword: string }) => {
 
     if (places.length) {
       setPlaces(places);
+
+      const { LatLngBounds } = (await google.maps.importLibrary(
+        "core"
+      )) as google.maps.CoreLibrary;
+      const bounds = new LatLngBounds();
+
+      places.forEach((place) => {
+        if (place.location) bounds.extend(place.location);
+      });
+
+      map?.setCenter(bounds.getCenter());
+    } else {
+      console.log("No results");
     }
   };
 
@@ -56,7 +68,7 @@ export const Map = ({ keyword }: { keyword: string }) => {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        setCenter(myLocation);
+        map?.setCenter(myLocation);
         map?.setZoom(16);
       });
     }
@@ -73,7 +85,7 @@ export const Map = ({ keyword }: { keyword: string }) => {
       <div className="w-full h-full grow flex">
         <GoogleMap
           mapContainerStyle={defaultMapContainerStyle}
-          center={center || defaultMapCenter}
+          center={defaultMapCenter}
           zoom={defaultMapZoom}
           options={defaultMapOptions}
           onLoad={onLoad}
