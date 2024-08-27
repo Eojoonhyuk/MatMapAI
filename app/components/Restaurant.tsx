9af653433187248b9a64a046c90bb474b7875491
Eojoonhyuk <anftlcb@gmail.com>
2024-08-28 01:04:29 +0900

    chore: 음식점 default image 추가

--- app/components/Restaurant.tsx
+++ app/components/Restaurant.tsx
@@ -9,17 +9,19 @@ export const Restaurant = ({
     placeInformation;
   return (
     <div>
-      {photos && photos.length > 0 && (
-        <div style={{ width: "340px", maxHeight: "150px", overflow: "hidden" }}>
-          <Image
-            src={photos[0].getURI()}
-            width={300}
-            height={200}
-            alt="장소 이미지"
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
-          />
-        </div>
-      )}
+      <div className="min-w-80 max-h-36 overflow-hidden">
+        <Image
+          className="w-full h-full object-cover"
+          src={
+            photos && photos.length > 0
+              ? photos[0].getURI()
+              : "/default/place.png"
+          }
+          width={300}
+          height={200}
+          alt="장소 이미지"
+        />
+      </div>
       <div className="flex flex-col gap-2 p-2">
         <p className="text-lg font-semibold">{displayName}</p>
         <p className="font-medium">주소: {formattedAddress}</p>
