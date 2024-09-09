0a302fb17ff46501697f2879b79204a1c9badac1
Eojoonhyuk <anftlcb@gmail.com>
2024-09-09 21:01:04 +0900

    style: 스켈레톤 추가

--- app/components/Restaurant.tsx
+++ app/components/Restaurant.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { FaStar } from "react-icons/fa";
+import { Skeleton } from "./Skeleton";
+import { useState } from "react";
 
 export const Restaurant = ({
   placeInformation,
@@ -8,28 +10,36 @@ export const Restaurant = ({
 }) => {
   const { photos, displayName, formattedAddress, rating, userRatingCount } =
     placeInformation;
+
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div>
-      <div className="w-80 h-36">
+    <div className="w-80">
+      {isLoading && <Skeleton />}
+      <div className={`w-full ${isLoading ? "h-0" : "h-36"}`}>
         <Image
           className="w-full h-full"
           src={
-            photos && photos.length > 0
-              ? photos[0].getURI()
-              : "/default/place.png"
+            (photos && photos[0] && photos[0].getURI()) ||
+            "https://maps.gstatic.com/tactile/pane/default_geocode-2x.png"
           }
           width={300}
           height={200}
           alt="장소 이미지"
+          onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
-      <div className="w-80 flex flex-col gap-2 p-2">
+      <div
+        className={`w-full flex-col gap-2 p-2 ${isLoading ? "hidden" : "flex"}`}
+      >
         <p className="text-lg font-semibold">{displayName}</p>
         <p className="font-medium">{formattedAddress}</p>
         <div className="flex gap-1 font-medium">
           <FaStar color="yellow" />
-          <span>{rating}</span>
-          <span>({userRatingCount})</span>
+          <div>
+            <span>{rating}</span>
+            <span>({userRatingCount})</span>
+          </div>
         </div>
       </div>
     </div>
